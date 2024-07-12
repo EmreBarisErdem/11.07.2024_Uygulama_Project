e87fe0f435eed12f634510525b0586f80b5fa611
Emre Baris Erdem <erdem.emre.baris@gmail.com>
2024-07-12 13:22:28 +0300

    gerekli değişiklikler yapıldı.

--- MyReactProject/src/App.jsx
+++ MyReactProject/src/App.jsx
@@ -1,21 +1,54 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Header from './components/Header'
 import Main from './components/Main'
 import CardList from './components/CardList'
-import fakeRecipes from './assets/js/recipes'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
-  const [tarifler,setTarifler] = useState(fakeRecipes);
+  const [tarifler,setTarifler] = useState([]);
 
   const addRecipe = (yeniTarif) => {
-    setTarifler(prev=>[...prev,yeniTarif]);
+    const newRecipe = postAsync(yeniTarif);
+    setTarifler(prev=>[...prev,newRecipe]);
   }
 
   const deleteRecipe = (id) => {
-    setTarifler(prev=>prev.filter(statedenGelen=>statedenGelen.id !== id));
+    deleteByIDAsync(id);
+    // setTarifler(prev=>prev.filter(statedenGelen=>statedenGelen.id !== id));
+    setTarifler(prev => prev.filter)
   }
+
+  const getAsync = async () => {
+    const url = "http://localhost:3000/fakeRecipes"
+    const response = await fetch(url);
+    const tarifler = await response.json();
+    setTarifler(tarifler);
+  }
+
+  useEffect(()=>{
+    getAsync();
+  },[])
+
+
+  const postAsync = async (newPost) => {
+    const response = await fetch("http://localhost:3000/fakeRecipes", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newPost)
+    })
+    const data = await response.json();
+    return data
+  }
+
+  const deleteByIDAsync = async (ID) => {
+    const response = await fetch(`"http://localhost:3000/fakeRecipes/${ID}"`,{
+        method:"DELETE",
+    })
+    const data = await response.json()
+    return data
+  }
+
   return (
     <>
       <Header/>
--- MyReactProject/src/components/Main.jsx
+++ MyReactProject/src/components/Main.jsx
@@ -1,41 +1,89 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const Main = ({addRecipe,tarifler}) => {
-  const [recipeTitle,setRecipeTitle] = useState("");
-  const [recipeDescription,setRecipeDescription] = useState("");
-  const [recipeImageURL,setImageURL] = useState("");
+const Main = ({ addRecipe, tarifler }) => {
+  const [recipeTitle, setRecipeTitle] = useState("");
+  const [recipeDescription, setRecipeDescription] = useState("");
+  const [recipeImageURL, setImageURL] = useState("");
+  const [titleError, setTitleError] = useState(true);
+  const [descriptionError, setDescriptionError] = useState(true);
+  const [imageURLError, setImageURLError] = useState(true);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     
-    addRecipe({
-      id: tarifler.length === 0 ? "1" : (Number(tarifler[tarifler.length-1].id)+1).toString(),
-      title: recipeTitle,
-      description: recipeDescription,
-      image: recipeImageURL
-    })
+    if (recipeTitle.trim() && recipeDescription.trim() && recipeImageURL.trim())
+    {
+      
+      setTitleError(true);
+      setImageURLError(true);
+      setDescriptionError(true);
+      
+      
+      addRecipe({
+        id:
+        tarifler.length === 0 ? "1" : (Number(tarifler[tarifler.length - 1].id) + 1).toString(),
+        title: recipeTitle,
+        description: recipeDescription,
+        image: recipeImageURL,
+      });
+      
+    } 
+    else
+    {
 
-  }
+      if(!recipeTitle.trim())
+      {
+        setTitleError(false);
+      }
+      if(!recipeDescription.trim())
+      {
+        setDescriptionError(false);
+      }
+      if(!recipeImageURL.trim())
+      {
+        setImageURLError(false);
+      }
 
+    }
+  };
+  // setRecipeTitle("");
+  // setRecipeDescription("");
+  // setImageURL("");
   return (
-    
-      <main>
-          <h2>Welcome to My Recipe Sharing Platform</h2>
-          <p>Lorem ipsum dolor sit amet.</p>
+    <main>
+      <h2>Welcome to My Recipe Sharing Platform</h2>
+      <p>Lorem ipsum dolor sit amet.</p>
 
-        <form onSubmit={handleSubmit}>
-          <input onChange={(e)=>setRecipeTitle(e.target.value)} type="text" placeholder='Recipe Title'/>
-          <textarea onChange={(e)=>setRecipeDescription(e.target.value)} placeholder='Recipe Description'></textarea>
-          <input onChange={(e)=>setImageURL(e.target.value)} type="url" placeholder='Image URL' />
-          <button type='submit'>Add Recipe</button>
-        </form>
-  
+      <form onSubmit={handleSubmit}>
+        <input
+          value={recipeTitle}
+          onChange={(e) => setRecipeTitle(e.target.value)}
+          type="text"
+          placeholder="Recipe Title"
+        />
+        {titleError ? <p></p> : <p>You must enter a recipe title!</p>}
+        <textarea
+          value={recipeDescription}
+          onChange={(e) => setRecipeDescription(e.target.value)}
+          placeholder="Recipe Description"
+        ></textarea>
+        {descriptionError ? (
+          <p></p>
+        ) : (
+          <p>You must enter a recipe description!</p>
+        )}
+        <input
+          value={recipeImageURL}
+          onChange={(e) => setImageURL(e.target.value)}
+          type="url"
+          placeholder="Image URL"
+        />
+        {imageURLError ? <p></p> : <p>You must enter a image url!</p>}
+        <button type="submit">Add Recipe</button>
+      </form>
+    </main>
+  );
+};
 
-
-
-      </main>
-   
-  )
-}
-
-export default Main
+export default Main;
